a0dc0488b1ee09f12bfff07dc0b396f697fad371
S.Mojtaba Banaie <mojtaba.banaie@gmail.com>
2015-12-12 23:30:16 +0330

    Update GenerateTags.py

--- GenerateTags.py
+++ GenerateTags.py
@@ -1,29 +1,21 @@
 ﻿# -*- coding: utf-8 -*-
-
-
 import requests
 import codecs
 from utility import MakeUniqueValues
-
 from langdetect import detect
 
 client_id = 'baad7b9aa14041c5bb2dc1f9b477447c'
-
 output = codecs.open(r'D:\MyProjects\Instagram\tags.txt', 'wU', 'utf-8')
-
 parameters = dict(
     client_id=client_id
 )
 
 tags =["زمستان","مشهد" ,"ایران" ,"طبیعت" ,"رفیق" ,"کتاب" , "زندگی","روزگار"]
-
 for tag in tags :
     tag_endpoint = "https://api.instagram.com/v1/tags/%s/media/recent" % (tag)
     try :
         r = requests.get(tag_endpoint,params=parameters)
-
         if r.status_code == 200 :
-
             medias =  r.json()["data"]
             for media in medias:
                 for tag in media["tags"] :
@@ -32,6 +24,5 @@ for tag in tags :
     except :
         pass
 output.close()
-
 # remove redundancies
 MakeUniqueValues('D:\MyProjects\Instagram\tags.txt')
