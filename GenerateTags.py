5b53026907e73678c9eb4a6ca0a66dccb85c24c8
S.Mojtaba Banaie <mojtaba.baanie@gmail.com>
2015-12-14 13:46:16 +0330

    Get More Info

--- GenerateTags.py
+++ GenerateTags.py
@@ -5,14 +5,25 @@ from utility import MakeUniqueValues
 from langdetect import detect
 
 client_id = 'baad7b9aa14041c5bb2dc1f9b477447c'
-output = codecs.open(r'tags.txt', 'aU', 'utf-8')
 parameters = dict(
     client_id=client_id
 )
 
-tags =["زمستان","مشهد" ,"ایران" ,"گل","تهران","اصفهان","شیراز","اهواز","بوشهر","پسرونه","دخترونه","طنز","تلگرام"]
+
+tags_file = codecs.open(r'tags.txt', 'rU', 'utf-8')
+tags = []
+for tag in tags_file :
+    tags.append(tag)
+tags_file.close()
+
+output = codecs.open(r'tags.txt', 'aU', 'utf-8')
+
+tags_collection = {}
+
+# tags =["زمستان","مشهد" ,"ایران" ,"گل","تهران","اصفهان","شیراز","اهواز","بوشهر","پسرونه","دخترونه","طنز","تلگرام"]
 for tag in tags :
-    tag_endpoint = "https://api.instagram.com/v1/tags/%s/media/recent" % (tag)
+    tag_endpoint = "https://api.instagram.com/v1/tags/%s/media/recent" % (tag.strip())
+    print tag_endpoint
     try :
         r = requests.get(tag_endpoint,params=parameters)
         if r.status_code == 200 :
@@ -21,9 +32,15 @@ for tag in tags :
                 for tag in media["tags"] :
                         if detect(tag)=='fa' and len(tag)>1:
                             print(tag)
-                            output.write(tag + "\n")
+                            if tag in tags_collection.keys() :
+                                tags_collection[tag] += 1
+                            else :
+                                tags_collection[tag] = 1
+
     except :
         pass
+for k,v in tags_collection.items() :
+    output.write("%s - %s\n" %(k,v) )
 output.close()
 # remove redundancies
 MakeUniqueValues('tags.txt')
